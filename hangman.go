adb761080ca20138fbd4f9f4902065ad8265b3c5
D0ria <doriaremadna@gmail.com>
2022-10-07 11:02:52 +0200

    fonction qui lit chaque mot de words.txt

--- hangman.go
+++ hangman.go
@@ -1,8 +1,23 @@
-<<<<<<< HEAD
 package hangman
-=======
-package main
 
 import (
 	"bufio"
->>>>>>> 6bc06801f3c8a003aea70814b13a06e7651793cf
+	"fmt"
+	"os"
+)
+
+func Reader() {
+	f, err := os.Open("words.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer f.Close()
+	scanner := bufio.NewScanner(f)
+	scanner.Split(bufio.ScanWords)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Println(err)
+	}
+}
--- main.go
+++ main.go
@@ -5,5 +5,5 @@ import (
 )
 
 func main() {
-	fmt.Println(hangman.affiche())
+	fmt.Println(hangman.Reader())
 }
