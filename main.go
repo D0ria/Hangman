85b6a615e7195ff5485f58c91f5b5aab676b53cd
D0ria <doriaremadna@gmail.com>
2022-10-11 11:27:09 +0200

    modif main

--- main.go
+++ main.go
@@ -5,5 +5,14 @@ import (
 )
 
 func main() {
+
+	var hangman Game
+	hangman.Word = RandomWord
+	hangman.ToFind = HiddenWord
+	hangman.Attempts = 3
+	hangman.positions = Step()
+	hangman.Found = false
+
 	fmt.Printf(RevealFewLetters(letter))
+	fmt.Println(hangman)
 }
