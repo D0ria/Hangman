3b938961a17560e02a2026552562287ae1495248
D0ria <doriaremadna@gmail.com>
2022-10-13 09:25:36 +0200

    main.go

--- main.go
+++ main.go
@@ -1,11 +1,1 @@
 package hangman
-
-func main() {
-
-	var morigno Game
-	morigno.Word = RandomWord
-	morigno.ToFind = HiddenWord
-	morigno.Attempts = 3
-	morigno.HangmanPositions = Step()
-	morigno.Found = false
-}
